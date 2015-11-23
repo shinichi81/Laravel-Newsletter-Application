59ce734eaa12c965dcf34fc55b90eb4f9a73ab30
Rasheeda Yehuza <rashkeed@gmail.com>
2015-11-23 21:30:36 +0000

    Group routes and use middleware for auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,23 +15,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::group(['middleware' => 'auth'], function () {
+
+	Route::get('logout', 'Auth\AuthController@getLogout');
+
+	Route::get('logout', 'Auth\AuthController@getLogout');
+
+	Route::get('home', function () {
+	    return Redirect::to("dashboard");
+	});
+
+	Route::get('dashboard', function () {
+	    return "dashboard!";
+	});
+
+	Route::get("test", 'Tests\TestController@getIndex');
+	Route::get("subscribers", 'SubscribersController@getIndex');
+
+});
+
 // Authentication routes...
 Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
-Route::get('logout', 'Auth\AuthController@getLogout');
-
+	
 // Registration routes...
 Route::get('register', 'Auth\AuthController@getRegister');
 Route::post('register', 'Auth\AuthController@postRegister');
 
-Route::get('logout', 'Auth\AuthController@getLogout');
-
-Route::get('home', function () {
-    return Redirect::to("dashboard");
+Route::get('auth/login', function () {
+	 return Redirect::to("login");
 });
 
-Route::get('dashboard', function () {
-    return "dashboard!";
-});
 
-Route::get("test", 'Tests\TestController@getIndex');

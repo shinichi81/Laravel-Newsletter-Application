d2ce6c47576f58ee5c1fbec21a1848f76c17eeb1
Rasheeda Yehuza <rashkeed@gmail.com>
2015-11-29 21:22:39 +0000

    Client specific: manual template and model

---
+++ app/Templates/EPITemplate.php
@@ -0,0 +1,9 @@
+<?php namespace Newsletter\Templates;
+
+use Eloquent;
+use Newsletter\Templates\DefaultTemplate;
+
+class EPITemplate extends Eloquent{
+
+	protected $table = 'epi_newsletter_templates';
+}

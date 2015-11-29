951e63237c4719a192c5f653315b5c97dd482795
Rasheeda Yehuza <rashkeed@gmail.com>
2015-11-29 21:07:45 +0000

    Process and save messages in queued table

--- app/Http/Controllers/NewslettersController.php
+++ app/Http/Controllers/NewslettersController.php
@@ -7,6 +7,7 @@ use Auth;
 use Redirect;
 use Newsletter\Groups\Group;
 use Newsletter\Subscribers\SusbcriberGroup;
+use DB;
 
 class NewslettersController extends Controller{
 
@@ -18,8 +19,50 @@ class NewslettersController extends Controller{
 				->with("groups", $groups);
 	}
 
-	public function postCreate(){
+	public function postSend(){
 
+		$data = Input::get();
+
+		if(isset($data['manual_emails'])){
+
+			if(strlen($data['manual_emails']) >5 ){
+
+				$emails = explode(",", $data['manual_emails']);
+			}
+
+		}
+
+		if(isset($data['groups'])){
+
+			$group = Group::find($data['groups']);
+
+			if($group){
+
+				$subscribers = DB::table('subscriber_groups')
+		            ->join('subscribers', 'subscribers.id', '=', 'subscriber_groups.subscriber_id')
+		            ->select('subscribers.email')
+		            ->get(); 
+
+		        foreach ($subscribers as $key => $email) {
+		        	
+		        	$emails[] = $email;
+		        }
+
+			}
+
+		}
+
+		foreach ($emails as $key => $email) {
+			
+			$queueMessage = new QueuedMessage;
+			$queueMessage->user_id = Auth::user()->id;
+			$queueMessage->template = "templates.epi";
+			$queueMessage->email = $email;
+			$queueMessage->save();
+		}
 
 	}
+
+
+
 }
